fef2165b14d4d854c43efd46a6799403f242109e
David Vossel <davidvossel@gmail.com>
2025-03-27 10:42:55 -0400

    Handle custom roles for intra namespace folders
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- internal/controller/namespacedfolder_controller.go
+++ internal/controller/namespacedfolder_controller.go
@@ -18,11 +18,16 @@ package controller
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	rbacv1 "k8s.io/api/rbac/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -45,7 +50,7 @@ func (r *NamespacedFolderReconciler) getAllVMs(ctx context.Context, folder *v1al
 	var vms []string
 	for _, vmName := range folder.Spec.VirtualMachines {
 		vm := virtv1.VirtualMachine{}
-		name := client.ObjectKey{Name: vmName}
+		name := client.ObjectKey{Name: vmName, Namespace: folder.Namespace}
 		err := r.Client.Get(ctx, name, &vm)
 		if err != nil {
 			if !apierrors.IsNotFound(err) {
@@ -80,6 +85,141 @@ func (r *NamespacedFolderReconciler) getAllVMs(ctx context.Context, folder *v1al
 	return vms, nil
 }
 
+func generateRoleNameHash(folderUID types.UID, namespace string, rules []rbacv1.PolicyRule) (string, error) {
+
+	rulesJson, err := json.Marshal(rules)
+	if err != nil {
+		return "", err
+	}
+
+	s := fmt.Sprintf("%s-%s-%s", string(folderUID), namespace, string(rulesJson))
+	h := sha256.New()
+
+	_, err = h.Write([]byte(s))
+	if err != nil {
+		return "", err
+	}
+
+	bs := h.Sum(nil)
+
+	return hex.EncodeToString(bs), nil
+}
+
+func (r *NamespacedFolderReconciler) reconcileRole(ctx context.Context, folder *v1alpha1.NamespacedFolder, roleRef *rbacv1.RoleRef, vms []string) (string, error) {
+
+	// TODO - decide if it's a cluster role or role
+	// get the rules
+	// filter rules for virtualmachine stuff and use named resource list
+	// make sure list/watch for all vms in namespace
+	// create a new role and add ownership label
+	// return the name of the role
+
+	var rules []rbacv1.PolicyRule
+
+	if roleRef.Kind == "Role" {
+		role := &rbacv1.Role{}
+		name := client.ObjectKey{Name: roleRef.Name, Namespace: folder.Namespace}
+
+		if err := r.Client.Get(ctx, name, role); err != nil {
+			if apierrors.IsNotFound(err) {
+				return "", nil
+			}
+			return "", err
+		}
+
+		rules = role.Rules
+	} else if roleRef.Kind == "ClusterRole" {
+		clusterRole := &rbacv1.ClusterRole{}
+		name := client.ObjectKey{Name: roleRef.Name}
+
+		if err := r.Client.Get(ctx, name, clusterRole); err != nil {
+			if apierrors.IsNotFound(err) {
+				return "", nil
+			}
+			return "", err
+		}
+
+		rules = clusterRole.Rules
+	} else {
+		// unknown kind, ignore
+		return "", nil
+	}
+
+	//filter rules and add resource names to them
+	newRules := []rbacv1.PolicyRule{}
+
+	for _, rule := range rules {
+		foundGroups := []string{}
+
+		if len(rule.ResourceNames) != 0 {
+			// Ignoring rules with specific resource names right now.
+			// TODO make this compatible with roles with ResourceNames by
+			// merging resourceNames with vm names
+			continue
+		}
+		for _, group := range rule.APIGroups {
+			if group == "kubevirt.io" || group == "subresources.kubevirt.io" {
+				foundGroups = append(foundGroups, group)
+			}
+		}
+
+		foundResources := []string{}
+		for _, resource := range rule.Resources {
+			baseResource := strings.Split(resource, "/")[0]
+			if resource == "*" ||
+				baseResource == "virtualmachineinstances" ||
+				baseResource == "virtualmachines" {
+
+				foundResources = append(foundResources, resource)
+			}
+		}
+
+		if len(foundGroups) == 0 || len(foundResources) == 0 {
+			continue
+		}
+
+		newRule := rbacv1.PolicyRule{
+			Verbs:         rule.Verbs,
+			APIGroups:     foundGroups,
+			Resources:     foundResources,
+			ResourceNames: vms,
+		}
+
+		newRules = append(newRules, newRule)
+
+	}
+
+	if len(newRules) == 0 {
+		return "", nil
+	}
+
+	roleName, err := generateRoleNameHash(folder.UID, folder.Namespace, newRules)
+
+	if err != nil {
+		return "", err
+	}
+
+	newRole := &rbacv1.Role{}
+	newRole.Name = roleName
+	newRole.Namespace = folder.Namespace
+
+	_, err = controllerutil.CreateOrUpdate(ctx, r.Client, newRole, func() error {
+		if newRole.Labels == nil {
+			newRole.Labels = map[string]string{}
+		}
+		newRole.Labels[NamespacedFolderOwnershipLabel] = string(folder.UID)
+
+		newRole.Rules = newRules
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return roleName, nil
+
+}
+
 func (r *NamespacedFolderReconciler) reconcileFolderPermissions(ctx context.Context, folder *v1alpha1.NamespacedFolder, vms []string) ([]string, []string, error) {
 
 	appliedRoleBindings := []string{}
@@ -87,17 +227,29 @@ func (r *NamespacedFolderReconciler) reconcileFolderPermissions(ctx context.Cont
 	namespace := folder.Namespace
 
 	for _, fp := range folder.Spec.FolderPermissions {
-		for _, rr := range fp.RoleRefs {
+		for _, existingRR := range fp.RoleRefs {
+
+			roleName, err := r.reconcileRole(ctx, folder, &existingRR, vms)
+
+			if err != nil {
+				return appliedRoleBindings, appliedRoles, err
+			} else if roleName == "" {
+				// role isn't related to virtual machines
+				continue
+			}
+
+			appliedRoles = append(appliedRoles, roleName)
+			rr := rbacv1.RoleRef{
+				APIGroup: "rbac.authorization.k8s.io",
+				Kind:     "Role",
+				Name:     roleName,
+			}
+
 			name, err := generateRoleBindingNameHash(folder.UID, namespace, fp.Subject, rr)
 			if err != nil {
 				return appliedRoleBindings, appliedRoles, err
 			}
 
-			// TODO, Limit the binding to only the VMs in question.
-			// This involves the following
-			// 1. look up rolerefs, and make a custom role for each one where only the virtualmachine resources are kept
-			// 2. modify each rule in the custom role to only target the named VMs
-			// 3. explicitly allow 'list' for all VMs in namespace though, in order for folder UI to work.
 			expectedRoleBinding := &rbacv1.RoleBinding{}
 			expectedRoleBinding.Name = name
 			expectedRoleBinding.Namespace = namespace
@@ -120,7 +272,6 @@ func (r *NamespacedFolderReconciler) reconcileFolderPermissions(ctx context.Cont
 	}
 
 	return appliedRoleBindings, appliedRoles, nil
-
 }
 
 // +kubebuilder:rbac:groups=kubevirtfolderview.kubevirt.io.github.com,resources=namespacedfolders,verbs=get;list;watch;create;update;patch;delete
