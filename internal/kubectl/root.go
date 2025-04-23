bdc4d5b439ac69075d2cc9a0527d1a3d61bffa5b
David Vossel <davidvossel@gmail.com>
2025-04-23 15:46:41 -0400

    Only display namespaces in tree view cli that exist
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- internal/kubectl/root.go
+++ internal/kubectl/root.go
@@ -10,12 +10,16 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	v1alpha1 "github.com/davidvossel/kubevirt-folder-view/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	virtv1 "kubevirt.io/api/core/v1"
 )
 
 var rootCmd *cobra.Command
 
 func init() {
 	utilruntime.Must(v1alpha1.AddToScheme(scheme.Scheme))
+	utilruntime.Must(virtv1.AddToScheme(scheme.Scheme))
+	utilruntime.Must(corev1.AddToScheme(scheme.Scheme))
 
 	rootCmd = &cobra.Command{
 		Use: "folder-view",
--- internal/kubectl/tree.go
+++ internal/kubectl/tree.go
@@ -8,13 +8,23 @@ import (
 
 	v1alpha1 "github.com/davidvossel/kubevirt-folder-view/api/v1alpha1"
 	"github.com/spf13/cobra"
+	corev1 "k8s.io/api/core/v1"
+	virtv1 "kubevirt.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	//corev1 "k8s.io/api/core/v1"
 )
 
-func printTree(root *v1alpha1.FolderIndex,
-	childParent map[string]string,
+type printTreeData struct {
+	root           *v1alpha1.FolderIndex
+	childParentMap map[string]string
+	vmToFolderMap  map[string]string
+
+	vmNamespaceMap map[string][]string
+	namespaceMap   map[string]struct{}
+	vmMap          map[string]struct{}
+}
+
+func printTree(data *printTreeData,
 	parentClusterFolder string,
 	parentNamespace string,
 	parentNamespacedFolder string,
@@ -22,7 +32,7 @@ func printTree(root *v1alpha1.FolderIndex,
 
 	if parentClusterFolder != "" {
 		fmt.Printf("%s* ClusterFolder: [%s]\n", indention, parentClusterFolder)
-		node, ok := root.Spec.ClusterFolderEntries[parentClusterFolder]
+		node, ok := data.root.Spec.ClusterFolderEntries[parentClusterFolder]
 		if !ok {
 			return
 		}
@@ -30,36 +40,54 @@ func printTree(root *v1alpha1.FolderIndex,
 		indention = fmt.Sprintf("%s  ", indention)
 
 		for _, ns := range node.Namespaces {
-			printTree(root, childParent, "", ns, "", indention)
+			printTree(data, "", ns, "", indention)
 		}
 		for _, child := range node.ChildFolders {
-			printTree(root, childParent, child, "", "", indention)
+			printTree(data, child, "", "", indention)
 		}
 	} else if parentNamespace != "" {
+		_, exists := data.namespaceMap[parentNamespace]
+		if !exists {
+			return
+		}
 		fmt.Printf("%s* Namespace: [%s]\n", indention, parentNamespace)
 		namespaceParentKey := fmt.Sprintf("NAMESPACE:%s", parentNamespace)
-		indention = fmt.Sprintf("%s  ", indention)
-		for childFolder, parentNS := range childParent {
+		for childFolder, parentNS := range data.childParentMap {
 			if parentNS != namespaceParentKey {
-				continue
+				return
 			}
-			printTree(root, childParent, "", "", childFolder, indention)
+			printTree(data, "", "", childFolder, indention+"  ")
 		}
+
+		vmList, exists := data.vmNamespaceMap[parentNamespace]
+		if !exists {
+			return
+		}
+		for _, vm := range vmList {
+			_, isInFolder := data.vmToFolderMap[vm]
+			if !isInFolder {
+				fmt.Printf("%s* Namespace: [%s]\n", indention, parentNamespace)
+			}
+
+		}
+
 	} else if parentNamespacedFolder != "" {
-		node, ok := root.Spec.NamespacedFolderEntries[parentNamespacedFolder]
+		node, ok := data.root.Spec.NamespacedFolderEntries[parentNamespacedFolder]
 		if !ok {
 			return
 		}
 
 		fmt.Printf("%s* NamespacedFolder: [%s]\n", indention, parentNamespacedFolder)
 
-		indention = fmt.Sprintf("%s  ", indention)
-
 		for _, vm := range node.VirtualMachines {
-			fmt.Printf("%s* VM: [%s]\n", indention+"  ", vm)
+			namespace := strings.Split(parentNamespacedFolder, "/")[0]
+			_, exists := data.vmMap[fmt.Sprintf("%s/%s", namespace, vm)]
+			if exists {
+				fmt.Printf("%s* VM: [%s]\n", indention+"  ", vm)
+			}
 		}
 		for _, child := range node.ChildFolders {
-			printTree(root, childParent, "", "", child, indention)
+			printTree(data, "", "", child, indention+"  ")
 		}
 	}
 }
@@ -70,10 +98,51 @@ var treeCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.Background()
 
+		childParentMap := map[string]string{}
+		rootClusterFolders := map[string]struct{}{}
+		rootNamespacedFolders := map[string]struct{}{}
+
+		vmToFolderMap := map[string]string{}
+		namespaceToFolderMap := map[string]string{}
+
+		// tells us if namespaces exist or not
+		namespaceMap := map[string]struct{}{}
+
+		// tells us if vms exist or not
+		vmMap := map[string]struct{}{}
+
+		// tells us if vms exist or not in a namespace
+		vmNamespaceMap := map[string][]string{}
+
 		cl, err := client.New(config.GetConfigOrDie(), client.Options{})
 		if err != nil {
-			fmt.Println("failed to create client")
+			fmt.Printf("failed to create client: %v\n", err)
 			os.Exit(1)
+		}
+
+		var vmList virtv1.VirtualMachineList
+		if err := cl.List(ctx, &vmList); err != nil {
+			fmt.Printf("failed to list vms: %v\n", err)
+			os.Exit(1)
+		}
+		for _, vm := range vmList.Items {
+			vmMap[fmt.Sprintf("%s/%s", vm.Namespace, vm.Name)] = struct{}{}
+
+			vmList, exists := vmNamespaceMap[vm.Namespace]
+			if exists {
+				vmNamespaceMap[vm.Namespace] = []string{vm.Name}
+			} else {
+				vmNamespaceMap[vm.Namespace] = append(vmList, vm.Name)
+			}
+		}
+
+		var namespaceList corev1.NamespaceList
+		if err := cl.List(ctx, &namespaceList); err != nil {
+			fmt.Printf("failed to list namespaces: %v\n", err)
+			os.Exit(1)
+		}
+		for _, ns := range namespaceList.Items {
+			namespaceMap[ns.Name] = struct{}{}
 		}
 
 		root := &v1alpha1.FolderIndex{}
@@ -83,19 +152,18 @@ var treeCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		childParent := map[string]string{}
-		rootClusterFolders := map[string]struct{}{}
-		rootNamespacedFolders := map[string]struct{}{}
-
 		// Discover the root folders
 		// start by discovering all child parent relationships
 		for parent, clusterEntry := range root.Spec.ClusterFolderEntries {
 			for _, child := range clusterEntry.ChildFolders {
-				childParent[child] = parent
+				childParentMap[child] = parent
 				delete(rootClusterFolders, child)
 			}
 
-			_, isChild := childParent[parent]
+			for _, ns := range clusterEntry.Namespaces {
+				namespaceToFolderMap[ns] = parent
+			}
+			_, isChild := childParentMap[parent]
 			if !isChild {
 				rootClusterFolders[parent] = struct{}{}
 			}
@@ -106,19 +174,30 @@ var treeCmd = &cobra.Command{
 			namespaceParentKey := fmt.Sprintf("NAMESPACE:%s", namespace)
 
 			for _, child := range namespacedEntry.ChildFolders {
-				childParent[child] = parent
+				childParentMap[child] = parent
 				delete(rootNamespacedFolders, child)
 			}
+			for _, vm := range namespacedEntry.VirtualMachines {
+				vmToFolderMap[vm] = parent
+			}
 
-			_, isChild := childParent[parent]
+			_, isChild := childParentMap[parent]
 			if !isChild {
 				rootNamespacedFolders[parent] = struct{}{}
-				childParent[parent] = namespaceParentKey
+				childParentMap[parent] = namespaceParentKey
 			}
 		}
 
 		for parent, _ := range rootClusterFolders {
-			printTree(root, childParent, parent, "", "", "")
+			printTree(&printTreeData{
+				root:           root,
+				childParentMap: childParentMap,
+				vmToFolderMap:  vmToFolderMap,
+				vmNamespaceMap: vmNamespaceMap,
+				vmMap:          vmMap,
+				namespaceMap:   namespaceMap,
+			}, parent, "", "", "")
+			//printNonFolderedNamespaces( )
 		}
 
 		// TODO print Namespaces and VMs within namespaces that are not in folders
